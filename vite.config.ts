ca1628d52825916b8ce13da3d4a0492edd676de0
r17x <ri7nz@evilfactory.id>
2023-11-21 23:54:05 +0700

    chore: setup base url by env variables

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.VITE_APP_BASE_URL || '/indiemart/',
+  base: process.env.VITE_APP_BASE_URL || '/',
   plugins: [
     million.vite({ auto: true }),
     react(),
