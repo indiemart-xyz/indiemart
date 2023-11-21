50af08c8ef1991976559807bfe8d314fecbb8116
r17x <ri7nz@evilfactory.id>
2023-11-21 23:35:58 +0700

    refactor(home): removed routings

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import { Suspense } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import Layout from '~/lib/layout';
-import Routings from '~/lib/router/Routings';
 import { theme } from '~/lib/styles/theme';
+
+const Home = React.lazy(() => import('~/lib/pages/home'));
 
 const App = () => (
   <ChakraProvider theme={theme}>
     <Router>
       <Layout>
-        <Routings />
+        <Suspense>
+          <Home />
+        </Suspense>
       </Layout>
     </Router>
   </ChakraProvider>
