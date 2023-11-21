49a820658c05e8befb65290bd14af04a93cf4f41
r17x <ri7nz@evilfactory.id>
2023-11-21 23:18:08 +0700

    chore: update build configuration for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.VITE_APP_BASE || '/',
   plugins: [
     million.vite({ auto: true }),
     react(),
